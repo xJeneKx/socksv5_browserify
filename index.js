f674a1639d49c5d1c588770e5ab235429dc969fa
xJeneKx <xxjenekxx@gmail.com>
2017-02-08 19:34:52 +0300

    browserify

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-var fs = require('fs'),
-    path = require('path');
-
-['server', 'client', 'Agents'].forEach(function(f) {
-  var exp = require(__dirname + '/lib/' + f),
-      keys = Object.keys(exp);
-  for (var i = 0, len = keys.length; i < len; ++i)
-    exports[keys[i]] = exp[keys[i]];
-});
-
-exports.auth = {};
-
-fs.readdirSync(__dirname + '/lib/auth').forEach(function(f) {
-  exports.auth[path.basename(f, '.js')] = require(__dirname + '/lib/auth/' + f);
-});
+exports.Server = require('./lib/server');
+exports.createServer = require('./lib/server').createServer;
+exports.Client = require('./lib/client');
+exports.connect = exports.createConnection = require('./lib/client').createConnection;
+exports.HttpAgent = require('./lib/Agents').HttpAgent;
+exports.HttpsAgent = require('./lib/Agents').HttpsAgent;
+exports.auth = {
+	None: require('./lib/auth/None'),
+	UserPassword: require('./lib/auth/UserPassword')
+};
